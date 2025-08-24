2636ff7b975e9813202f212c10903fb43404be79
Deepnshu Sharma <deepanshu0859@gmail.com>
2025-08-25 02:46:14 +0530

    vercel setup

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-const express  = require('express');
-const path = require('path')
-const urlRoute = require('./routes/url')
-const statisRoute = require("./routes/staticRouter")
-const { connectToDB } = require('./connect');
-const URL = require('./models/url');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const urlRoute = require("./routes/url");
+const statisRoute = require("./routes/staticRouter");
+const { connectToDB } = require("./connect");
+const URL = require("./models/url");
+require("dotenv").config();
 
 const app = express();
-const PORT = 8000;
 
 // mongodb connection
-connectToDB(process.env.MONGODB_URL).then(()=>{
+connectToDB(process.env.MONGODB_URL)
+  .then(() => {
     console.log("MongoDB connected!!!");
-});
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
+  
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 // ejs setup
-app.set("view engine","ejs")
-app.set("views",path.resolve("./views"))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 // routes
-app.use('/',statisRoute);
-app.use("/url",urlRoute);
-app.get("/:shortId",async (req,res)=>{
-    const shortId = req.params.shortId;
+app.use("/", statisRoute);
+app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
 
-    const entry = await URL.findOneAndUpdate({shortId},{
-        $push:{
-            visitHistory:{
-                timestamp:Date.now()
-            }
-        }
-    },{new:true});
+  const entry = await URL.findOneAndUpdate(
+    { shortId },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    },
+    { new: true }
+  );
 
-    res.redirect(entry.redirectURL);
-})
-
-
-app.listen(PORT , ()=>{
-    console.log(`Server started at Port: ${PORT}`);
-})
+  res.redirect(entry.redirectURL);
+});
